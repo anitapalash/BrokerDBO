44161d621b33a365f7b7139203dc34a00a016925
anitapalash <647316392@github.com>
2019-05-08 10:41:16 +0300

    initial commit

---
+++ Broker/Client.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Serialization;
+
+namespace Broker
+{
+    class Client
+    {
+        Type type;
+        Dictionary<string, int> criterias = new Dictionary<string,int>();
+        Dictionary<string, bool> features = new Dictionary<string, bool>();
+        public void setType(Type type)
+        {
+            this.type = type;
+        }
+
+        public Type getType()
+        {
+            return this.type;
+        }
+
+        public void setCriterias(Dictionary<string, int> map)
+        {
+            this.criterias = map;
+        }
+
+        public Dictionary<string, int> getCriterias()
+        {
+            return criterias; 
+        }
+
+        public void setFeatures(Dictionary<string, bool> map)
+        {
+            this.features = map;
+        }
+
+        public Dictionary<string, bool> getFeatures()
+        {
+            return features;
+        }
+    }
+}
---
+++ Broker/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Broker
+{
+    enum Type { INDIVIDUAL, LEGENT, CORPORATE }
+
+    static class Program
+    {
+        static Client client = new Client();
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Entrance());
+        }
+
+        public static void setClientType(Type type)
+        {
+            client.setType(type);
+        }
+
+        public static void setCriterias(Dictionary<string, int> map)
+        {
+            Dictionary<string, int> currentMap = client.getCriterias();
+            foreach (var rate in map)
+                currentMap.Add(rate.Key, rate.Value);
+            client.setCriterias(currentMap);
+        }
+
+        public static void setFeatures(Dictionary<string, bool> map)
+        {
+            Dictionary<string, bool> currentMap = client.getFeatures();
+            foreach (var feature in map)
+                currentMap.Add(feature.Key, feature.Value);
+            client.setFeatures(currentMap);
+        }
+    }
+}
